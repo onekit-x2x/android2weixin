6e2918688dee453e37f9db7bf67fdaf76215d9a3
15802303637 <>
2020-12-29 23:24:53 +0800

    .

--- miniprogram_dev/java/com/tencent/trtcsimpledemo/MainActivity.js
+++ miniprogram_dev/java/com/tencent/trtcsimpledemo/MainActivity.js
@@ -1,40 +1,50 @@
-import {OnekitPage} from '../../../../android2weixin/index'
-import AppCompatActivity from '../../../../android2weixin/java/androidx/appcompat/app/AppCompatActivity'
-import R from './R.js'
-
-class MainActivity {
-     onCreate( savedInstanceState) {
-    /*
-    new Handler().postDelayed(new Runnable() {
-      @Override
-      public void run() {
-          findViewById(R.id.launch_view).setVisibility(View.GONE);
-      }
-  }, 1000);*/
-  this.findViewById(R.id.bt_rtc).setOnClickListener({
-   onClick:( view) =>{
-          var intent = new Intent(MainActivity.this, RTCEntranceActivity.class);
-          startActivity(intent);
-      }
-  });
-  this. findViewById(R.id.bt_live).setOnClickListener( {
-   onClick:( view)=> {
-          var intent = new Intent(MainActivity.this, LiveRoomListActivity.class);
-          startActivity(intent);
-      }
-  });
-  this. findViewById(R.id.bt_screen).setOnClickListener({
-   onClick:( view)=> {
-          var intent = new Intent(MainActivity.this, ScreenEntranceActivity.class);
-          startActivity(intent);
-      }
-  });
-  this.findViewById(R.id.bt_custom_capture).setOnClickListener( {
- onClick:( view) =>{
-          var intent = new Intent(MainActivity.this, CustomEntranceActivity.class);
-          startActivity(intent);
-      }
-  });
-  }
+import {OnekitPage} from '../../../../android2weixin/index';
+import Intent from "../../../../android2weixin/java/android/content/Intent"
+import Bundle from "../../../../android2weixin/java/android/os/Bundle"
+import Handler from "../../../../android2weixin/java/android/os/Handler"
+import View from "../../../../android2weixin/java/android/view/View"
+import AppCompatActivity from "../../../../android2weixin/java/androidx/appcompat/app/AppCompatActivity"
+import CustomEntranceActivity from "../../../../java/com/tencent/custom/CustomEntranceActivity"
+import ScreenEntranceActivity from "../../../com/tencent/liteav/screen/ScreenEntranceActivity"
+import LiveRoomListActivity from "../../../com/tencent/live/LiveRoomListActivity"
+import RTCEntranceActivity from "../../../com/tencent/rtc/RTCEntranceActivity"
+class MainActivity extends AppCompatActivity{
+    onCreate(savedInstanceState ){
+        super.onCreate(savedInstanceState)
+        this.setContentView(R.layout.activity_main)
+        this.getSupportActionBar().hide()
+        new Handler().postDelayed({
+    run(){
+        this.findViewById(R.id.launch_view).setVisibility(View.GONE)
+    }
+},1000)
+        this.findViewById(R.id.bt_rtc).setOnClickListener({
+    onClick(view ){
+        var intent = new Intent(this,RTCEntranceActivity.class)
+        this.startActivity(intent)
+    }
+})
+        this.findViewById(R.id.bt_live).setOnClickListener({
+    onClick(view ){
+        var intent = new Intent(this,LiveRoomListActivity.class)
+        this.startActivity(intent)
+    }
+})
+        this.findViewById(R.id.bt_screen).setOnClickListener({
+    onClick(view ){
+        var intent = new Intent(this,ScreenEntranceActivity.class)
+        this.startActivity(intent)
+    }
+})
+        this.findViewById(R.id.bt_custom_capture).setOnClickListener({
+    onClick(view ){
+        var intent = new Intent(this,CustomEntranceActivity.class)
+        this.startActivity(intent)
+    }
+})
+    }
 }
-OnekitPage(new MainActivity());
+
+
+Page({})
+//OnekitPage(new MainActivity());
