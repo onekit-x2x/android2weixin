95285d80dc3167dd4cdd902f52f5fc6f41b4c27f
15802303637 <>
2021-01-13 12:49:31 +0800

    .

--- miniprogram_dev/index.js
+++ miniprogram_dev/index.js
@@ -4,9 +4,11 @@ import src from "./src/main/index"
 let result = {}
 result = OBJECT.assign(result,android2weixin)
 result = OBJECT.assign(result,src)
+/*
 result = OBJECT.assign(result,require("./customcapture/src/main/index"))
 result = OBJECT.assign(result,require("./debug/src/main/index"))
 result = OBJECT.assign(result,require("./live/src/main/index"))
 result = OBJECT.assign(result,require("./rtc/src/main/index"))
 result = OBJECT.assign(result,require("./screen/src/main/index"))
+*/
 module.exports = result
